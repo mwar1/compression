125ffbabc6b6d65e461be58cbb69ae8dc012d736
Max Waring <maximwaring@yahoo.co.uk>
2026-05-13 12:44:25 +0100

    BWT decoding and fix encoding to use cyclic shifts
    Closes #5

--- compressor_impls/bwt.cpp
+++ compressor_impls/bwt.cpp
@@ -9,8 +9,8 @@ std::vector<uint32_t> build_suffix_array(const std::vector<uint8_t>& txt) {
     uint32_t n = txt.size();
 
     std::vector<uint32_t> arr(n);
-    std::vector<int32_t> rank(n);
-    std::vector<int32_t> temp(n);
+    std::vector<uint32_t> rank(n);
+    std::vector<uint32_t> temp(n);
 
     std::iota(arr.begin(), arr.end(), 0);
     for (int i = 0; i < n; i++) rank[i] = txt[i];
@@ -18,9 +18,9 @@ std::vector<uint32_t> build_suffix_array(const std::vector<uint8_t>& txt) {
     for (int k = 1; k < n; k <<= 1) {
         auto cmp = [&](int i, int j) {
             if (rank[i] != rank[j]) return rank[i] < rank[j];
-            int ri = (i + k < n) ? rank[i + k] : -1;
-            int rj = (j + k < n) ? rank[j + k] : -1;
-            return ri < rj;
+            int ri = (i + k < n) ? (i + k) : (i + k - n);
+            int rj = (j + k < n) ? (j + k) : (j + k - n);
+            return rank[ri] < rank[rj];
         };
 
         std::sort(arr.begin(), arr.end(), cmp);
@@ -80,10 +80,47 @@ public:
     }
     
     std::vector<uint8_t> decode(const std::vector<uint8_t>& txt) const override {
-        std::vector<uint8_t> out;
-        if (txt.empty()) return out;
+        if (txt.empty()) return {};
+        
+        int data_len = txt.size() - 4;
+        std::vector<uint8_t> out(data_len);
+
+        // Read the primary index
+        uint32_t p_idx = 0;
+        for (int i=0; i<4; i++) {
+            uint8_t byte = txt.at(i);
+            p_idx |= (static_cast<uint32_t>(byte) << (i * 8));
+        }
+
+        const uint8_t* data = txt.data() + 4;
+
+        std::vector<int> count(256, 0);
+        for (int i = 0; i < data_len; i++) count[data[i]]++;
+
+        std::vector<int> first_col(256, 0);
+        int sum = 0;
+        for (int i = 0; i < 256; i++) {
+            first_col[i] = sum;
+            sum += count[i];
+            count[i] = 0;
+        }
+
+        // T maps the index in the last column to the index in the first column
+        std::vector<int> T(data_len);
+        for (int i = 0; i < data_len; i++) {
+            uint8_t c = data[i];
+            T[i] = first_col[c] + count[c];
+            count[c]++;
+        }
+
+        // Go backwards to reconstruct data
+        int curr = p_idx;
+        for (int i = data_len - 1; i >= 0; i--) {
+            out[i] = data[curr];
+            curr = T[curr];
+        }
     
-        return txt;
+        return out;
     }
 };
 
