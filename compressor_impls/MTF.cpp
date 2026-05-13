b2df69c9da85e74e0e4fef8ef2f4b120cbca3225
Max Waring <maximwaring@yahoo.co.uk>
2026-05-13 13:48:44 +0100

    Move-to-front transformation encoding

---
+++ compressor_impls/MTF.cpp
@@ -0,0 +1,53 @@
+#include "../compressor.h"
+
+#include <algorithm>
+#include <numeric>
+
+/*
+    Move-to-front transform
+*/
+class MTF : public Compressor {
+public:
+    std::vector<uint8_t> encode(const std::vector<uint8_t>& txt) const override {
+        std::vector<uint8_t> out;
+        if (txt.empty()) return {};
+
+        std::vector<uint8_t> alphabet(256, 0);
+        std::iota(alphabet.begin(), alphabet.end(), 0);
+
+        for (uint8_t b : txt) {
+            // Find the index of the character in the alphabet
+            uint8_t idx = 0;
+            for (int i = 0; i < 256; ++i) {
+                if (alphabet[i] == b) {
+                    idx = static_cast<uint8_t>(i);
+                    break;
+                }
+            }
+
+            out.push_back(idx);
+            
+            // Move this character to the front of the alphabet
+            for (int i = idx; i > 0; i--) {
+                alphabet[i] = alphabet[i - 1];
+            }
+            alphabet[0] = b;
+        }
+    
+        return out;
+    }
+    
+    std::vector<uint8_t> decode(const std::vector<uint8_t>& txt) const override {
+        std::vector<uint8_t> out;
+    
+        return txt;
+    }
+};
+
+static bool registered = []() {
+    Compressor::getRegistry()["MTF"] = {
+        []() { return std::make_unique<MTF>(); },
+        true
+    };
+    return true;
+}();
